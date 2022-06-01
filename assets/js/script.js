0d0e631e8dfa05ca103cd58e8a9cd45e7fe1f13d
taylordaveymoss <taylordaveymoss@gmail.com>
2022-06-01 11:50:53 -0400

    Added second API

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,4 +54,19 @@ $("document").ready(function() {
 			})
 			.catch(err => console.error(err))
 	}
+    
+    // API key
+    const icons = {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer X0vjEUN6KRlxbp2DoUkyHeM0VOmxY91rA6BbU5j3Xu6wDodwS0McmilLPBWDUcJ1'
+        }
+    };
+      
+    fetch('https://api.iconfinder.com/v4/iconsets?count=10', icons)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 })
+
